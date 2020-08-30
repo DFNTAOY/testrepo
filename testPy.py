8067ee1105ffd9b42c1ec0a9c17ecc46c61a2a22
DFNTAOY <ddfin@yahoo.com>
2020-08-29 18:18:56 -0700

    Create testPy.py
    Added a first practice python file

---
+++ testPy.py
@@ -0,0 +1,7 @@
+"""This is a Test Python file for Version Control Practice"""
+
+# Adding some python code
+
+def welcome(name):
+  name = input("Kindly specify your first name, please: ")
+  return "My name is {} and I am currently learning Version Control through Github Platform. Thank you".format(name)
